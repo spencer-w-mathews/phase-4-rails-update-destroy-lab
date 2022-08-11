1d0424c9e9acaecc578d6186262a1d71a87200a1
Spencer Mathews <smathews0906@gmail.com>
2022-08-11 11:08:53 -0500

    commit tree c3b7dc9942f4e66902d1385ab5fc7879772eeb90

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,5 @@
 class PlantsController < ApplicationController
-
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   # GET /plants
   def index
     plants = Plant.all
@@ -18,9 +18,38 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
-  private
-
-  def plant_params
-    params.permit(:name, :image, :price, :is_in_stock)
-  end
+    # PATCH /plants/:id
+    def update
+      plant = find_plant
+      plant.update(plant_params)
+      render json: plant
+    end
+  
+    # PATCH /plants/:id/like
+    def increment_likes
+      plant = find_plant
+      plant.update(likes: plant.likes + 1)
+      render json: plant
+    end
+  
+    # DELETE /plants/:id
+    def destroy
+      plant = find_plant
+      plant.destroy
+      head :no_content
+    end
+  
+    private
+  
+    def find_plant
+      Plant.find(params[:id])
+    end
+  
+    def plant_params
+      params.permit(:name, :price, :image, :is_in_stock)
+    end
+  
+    def render_not_found_response
+      render json: { error: "Plant not found" }, status: :not_found
+    end
 end
